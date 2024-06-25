d85fb38e75c4d62b910c9556cb30f968097c8b68
Tayne Bilyard <taynebilyard@gmail.com>
2024-06-25 22:48:49 +1200

    json file usable

--- slicer.js
+++ slicer.js
@@ -1,1 +1,7 @@
-const slicer = () => {}
+import transcript from './transcripts/oot.17-06-24.pworm-nest-jarrick.json' assert { type: 'json' }
+
+const slicer = (text) => {
+  console.log(text)
+}
+
+slicer(transcript.results.transcripts[0].transcript)
